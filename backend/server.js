c0e6eb6e34ad1596ad54ca6f46247a8823ecbbae
Gorang-Agarwal-03 <agarwalgorang7@gmail.com>
2025-10-30 23:34:33 +0530

    Simplify CORS configuration in server.js

--- backend/server.js
+++ backend/server.js
@@ -10,21 +10,10 @@ import geminiResponse from './gemini.js'
 dotenv.config()
 const app = express()
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://vertual-assistant-frontend.onrender.com"
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
-}));
+    origin:"https://vertual-assistant-frontend.onrender.com",
+    credentials: true
+}))
 
 const port = process.env.PORT || 5000 
 
